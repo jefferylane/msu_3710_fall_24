70ce259308aa5ca08723ec0dc20e4eb43436f76f
Jeffery Lane <jlane26@msudenver.edu>
2024-10-20 13:44:28 -0600

    added seed.rb file and that stuff to add 50 random people.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
-    has_one_attached :profile_photo
+    has_one_attached :profile_photo, dependent: :purge_later
     validates :school_email, uniqueness: { case_sensitive: false, message: "already taken."}
 
     VALID_MAJORS = ["Computer Engineering BS", "Computer Information Systems BS",
